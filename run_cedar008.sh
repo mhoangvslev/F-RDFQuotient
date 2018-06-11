747f6a041801d9c448bdd87bbba84c14ef8706a2
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2018-06-11 10:37:39 +0200

    Minor changes
    Former-commit-id: 702504e6750faa08431496ff29db77b14383341b

--- run_cedar008.sh
+++ run_cedar008.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong; do
-	./summarize.sh dblp/dblp.nt $SUMMARY_TYPE false
+	./summarize.sh dblp/dblp_large.nt $SUMMARY_TYPE false
 done
