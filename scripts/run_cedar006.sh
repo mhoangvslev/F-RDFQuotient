b5d17603cc384722767833d9e32776b69cf68372
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2018-06-13 16:11:26 +0200

    Changes to scripts for experiments
    Former-commit-id: bc969df82bc4665997d35171eb9b42b30156fa1b

--- scripts/run_cedar006.sh
+++ scripts/run_cedar006.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong onefb; do
+for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pwuf onefb; do
 	./summarize.sh bsbm/bsbm10m.nt $SUMMARY_TYPE false
 done
