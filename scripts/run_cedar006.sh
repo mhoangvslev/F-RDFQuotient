7336ca7b3901b5df34e97104f8227ef54e1421ce
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2018-06-14 11:28:59 +0200

    Transfering the experiments results and visualisations to paper repo, fixing run scripts for 2pwuf
    Former-commit-id: 5086fb275dc7eaa0b8df5aedd19e4b1cc2b1443a

--- scripts/run_cedar006.sh
+++ scripts/run_cedar006.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pwuf onefb; do
+for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb; do
 	./scripts/summarize.sh bsbm/bsbm10m.nt $SUMMARY_TYPE false
 done
