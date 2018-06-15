4981d16e863ccc5dc3519d53b7de1cb314748024
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2018-06-15 15:05:40 +0200

    Little changes to script
    Former-commit-id: 0836bfd8ac4217ac9334c79057c9e955761012bc

--- scripts/run_cedar001.sh
+++ scripts/run_cedar001.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb; do
-	for DATASET in dbpedia/dbpedia_persondata_en.nt springer/conference.nt nobel/nobel.nt insee/insee_geo.nt; do
+	for DATASET in dbpedia/dbpedia_persondata_en_uniq.nt springer/conference.nt nobel/nobel.nt insee/insee_geo.nt; do
 		./scripts/summarize.sh $DATASET $SUMMARY_TYPE false
 	done
 done
