eaa0d4ea93f05464180fe42c5e25f3cc192bebca
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2018-06-22 15:19:02 +0200

    Added shortcut scripts
    Former-commit-id: cc08ab559dc25df32dc18a7d867731a24c690f9a

--- scripts/run_cedar006_shortcut.sh
+++ scripts/run_cedar006_shortcut.sh
@@ -27,13 +27,13 @@ translate_summary_name() {
 for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb; do
 	# SATURATE + SUMMARIZE
 	# summarize saturated
-	./scripts/summarize.sh bsbm/bsbm100m.nt $SUMMARY_TYPE true
+	./scripts/summarize.sh bsbm/bsbm10m.nt $SUMMARY_TYPE true
 
 	# SHORTCUT
 	# summarize not saturated
-	./scripts/summarize.sh bsbm/bsbm100m.nt $SUMMARY_TYPE true
+	./scripts/summarize.sh bsbm/bsbm10m.nt $SUMMARY_TYPE true
 	# load the summary with saturation
-	./scripts/load.sh bsbm/bsbm100m_$(translate_summary_name($SUMMARY_TYPE)).nt true
+	./scripts/load.sh bsbm/bsbm10m_$(translate_summary_name($SUMMARY_TYPE)).nt true
 	# summarize saturated
-	./scripts/summarize.sh bsbm/bsbm100m_$(translate_summary_name($SUMMARY_TYPE)).nt $SUMMARY_TYPE true
+	./scripts/summarize.sh bsbm/bsbm10m_$(translate_summary_name($SUMMARY_TYPE)).nt $SUMMARY_TYPE true
 done
--- scripts/summarize.sh
+++ scripts/summarize.sh
@@ -2,6 +2,6 @@
 
 DATASET=$1
 SUMMARY_TYPE=$2
-SATURATE=$3
+SATURATED=$3
 
-java -Xmx100g -cp target/quotientSummaryWithDependencies-1.5-SNAPSHOT.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd summarize /data/datasets/$DATASET $SUMMARY_TYPE $SATURATE true true true | tee -a /data/datasets/$DATASET.log
+java -Xmx100g -cp target/quotientSummaryWithDependencies-1.5-SNAPSHOT.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd summarize /data/datasets/$DATASET $SUMMARY_TYPE $SATURATED true true true | tee -a /data/datasets/$DATASET.log
