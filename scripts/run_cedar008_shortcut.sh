b5df2ca86da14b46336a2efe15f9cd681df35b49
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2018-06-22 17:17:11 +0200

    Minor scripts fixes
    Former-commit-id: 9ae65b097d2646e453f5b76d31a5ddfe1d1b945a

--- scripts/run_cedar008_shortcut.sh
+++ scripts/run_cedar008_shortcut.sh
@@ -5,33 +5,23 @@ translate_summary_name() {
 		return "w"
 	elif [ "$1" = "strong" ]; then
 		return "s"
-	elif [ "$1" = "typedweak" ]; then
-		return "tw"
-	elif [ "$1" = "typedstrong" ]; then
-		return "ts"
 	elif [ "$1" = "2pweak" ]; then
 		return "2pw"
 	elif [ "$1" = "2pstrong" ]; then
 		return "2ps"
-	elif [ "$1" = "2ptypedweak" ]; then
-		return "2ptw"
-	elif [ "$1" = "2ptypedstrong" ]; then
-		return "2pts"
 	elif [ "$1" = "2pweakunionfind" ]; then
 		return "2pwuf"
-	elif [ "$1" = "onefb" ]; then
-		return "1fb"
 	fi
 }
 
-for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb; do
+for SUMMARY_TYPE in weak strong 2pweak 2pstrong 2pweakunionfind; do
 	# SATURATE + SUMMARIZE
 	# summarize saturated
 	./scripts/summarize.sh dblp/dblp_large.nt $SUMMARY_TYPE true
 
 	# SHORTCUT
 	# summarize not saturated
-	./scripts/summarize.sh dblp/dblp_large.nt $SUMMARY_TYPE true
+	#./scripts/summarize.sh dblp/dblp_large.nt $SUMMARY_TYPE false
 	# load the summary with saturation
 	./scripts/load.sh dblp/dblp_large_$(translate_summary_name $SUMMARY_TYPE).nt true
 	# summarize saturated
