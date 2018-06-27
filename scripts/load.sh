3383c875e3f0141089306b1643c039a95918ebf1
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2018-06-27 14:42:01 +0200

    Minor changes to scripts and gitignore
    Former-commit-id: 21c67c9ba13dcad57736c499b95cb872ccf1760e

--- scripts/load.sh
+++ scripts/load.sh
@@ -3,4 +3,4 @@
 DATASET=$1
 SATURATE=$2
 
-java -Xmx100g -cp target/quotientSummaryWithDependencies-1.5-SNAPSHOT.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd load /data/datasets/$DATASET $SATURATE true | tee -a /data/datasets/$DATASET.log
+java -Xmx80g -cp target/quotientSummaryWithDependencies-1.5-SNAPSHOT.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd load /data/datasets/$DATASET $SATURATE true | tee -a /data/datasets/$DATASET.log
