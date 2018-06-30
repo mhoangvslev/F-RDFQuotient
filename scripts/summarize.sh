2cb19e5edef9026d635d7bd1636636d74d8f48a8
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2018-06-30 09:58:52 +0200

    Publishing of the newest verion of the code (1.6)
    Former-commit-id: 34b31ebfacc8f9bf6689a13c0b8c2bdbea9bd3eb

--- scripts/summarize.sh
+++ scripts/summarize.sh
@@ -4,4 +4,4 @@ DATASET=$1
 SUMMARY_TYPE=$2
 SATURATED=$3
 
-java -Xmx90g -cp target/quotientSummaryWithDependencies-1.5-SNAPSHOT.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd summarize /data/datasets/$DATASET $SUMMARY_TYPE $SATURATED true true true | tee -a /data/datasets/$DATASET.log
+java -Xmx90g -cp target/quotientSummaryWithDependencies-1.6.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd summarize /data/datasets/$DATASET $SUMMARY_TYPE $SATURATED true true true | tee -a /data/datasets/$DATASET.log
