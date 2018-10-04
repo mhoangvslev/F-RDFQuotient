21a0bb8d4f98a8d8ec0d1d281db2e4817c8ea57c
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2018-10-04 11:29:37 +0200

    Mistake in scripts
    Former-commit-id: 8d522fd2ba6a7199e11300bcfb1189d65c28a12b

--- scripts/run_cedar006_SplitFoldLeaves.sh
+++ scripts/run_cedar006_SplitFoldLeaves.sh
@@ -3,7 +3,7 @@
 for DATASET in watdiv/watdiv100m.nt; do
 	for SATURATED in "true" "false"; do
 		for SUMMARY_TYPE in weak strong typedweak typedstrong onefb onefw; do
-			./scripts/summarizeSplitFoldLeaves.sh $DATASET $SUMMARY_TYPE $SATURATED foldleaves
+			./scripts/summarize.sh $DATASET $SUMMARY_TYPE $SATURATED foldleaves
 		done
 	done
 done
