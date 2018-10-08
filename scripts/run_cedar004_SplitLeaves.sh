e5b1efce76a2a4c639b8ca8dffeac1ca28411ae1
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2018-10-08 11:58:16 +0200

    Added scripts for split layout DOT drawings
    Former-commit-id: 84e85bafefac736c9b8eb853aa9d564d113bcd16

---
+++ scripts/run_cedar004_SplitLeaves.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for DATASET in watdiv/watdiv100m.nt; do
+	for SATURATED in "true" "false"; do
+		for SUMMARY_TYPE in weak strong typedweak typedstrong onefb onefw; do
+			./scripts/summarize.sh $DATASET $SUMMARY_TYPE $SATURATED splitleaves
+		done
+	done
+done
