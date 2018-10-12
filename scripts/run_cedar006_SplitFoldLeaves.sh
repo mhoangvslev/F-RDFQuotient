2ca7c683f267716493fac0aac7b01219906f1070
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2018-10-12 17:00:23 +0200

    Script modification
    Former-commit-id: 700f283eb77a43e0e43957481cf3471ef0084f36

--- scripts/run_cedar006_SplitFoldLeaves.sh
+++ scripts/run_cedar006_SplitFoldLeaves.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DATASET in bsbm/bsbm100m.nt; do
+for DATASET in bsbm/bsbm100m.nt bsbm/bsbm138m.nt; do
 	for SATURATED in "true" "false"; do
 		for SUMMARY_TYPE in weak strong typedweak typedstrong onefb onefw; do
 			./scripts/summarize.sh $DATASET $SUMMARY_TYPE $SATURATED foldleaves
