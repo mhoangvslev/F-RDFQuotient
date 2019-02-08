7cffa60af0f230cbedd5c4bdd4a3c528e9297484
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2019-02-08 11:45:50 +0100

    Adding licence placeholder file
    Former-commit-id: 16f873944043a6076c756f2e7dc209d69f94a99f

--- scripts/load.sh
+++ scripts/load.sh
@@ -3,4 +3,4 @@
 DATASET=$1
 SATURATE=$2
 
-java -Xmx90g -cp target/quotientSummaryWithDependencies-1.7-SNAPSHOT.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd load /data/datasets/$DATASET $SATURATE true | tee -a /data/datasets/$DATASET.log
+java -Xmx90g -cp target/quotientSummaryWithDependencies-1.8-SNAPSHOT.jar fr.inria.cedar.quotientSummary.controller.BuilderCmd load /data/datasets/$DATASET $SATURATE true | tee -a /data/datasets/$DATASET.log
