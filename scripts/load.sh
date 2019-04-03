21ac59d1c4879de24b6b211ea13cf20f0e94d5a3
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2019-04-03 14:01:39 +0200

    Update to the load.sh script
    Former-commit-id: 2260c0b52bdbfc94e70207b9555a047e6d367fd2

--- scripts/load.sh
+++ scripts/load.sh
@@ -4,5 +4,5 @@ DATASET=$1
 SATURATE=$2
 
 # memory allocation in JVM set to 90 GiB
-#java -Xmx90g -cp target/RDFQuotient-1.8-SNAPSHOT-with-dependencies.jar fr.inria.cedar.RDFQuotient.controller.Interface --load "dataset.filename=/data/datasets/$DATASET" | tee -a /data/datasets/$DATASET.log
-java -Xmx90g -jar target/RDFQuotient-1.8-SNAPSHOT-with-dependencies.jar --load "dataset.filename=/data/datasets/$DATASET" | tee -a /data/datasets/$DATASET.log
+#java -Xmx90g -cp target/RDFQuotient-1.8-SNAPSHOT-with-dependencies.jar fr.inria.cedar.RDFQuotient.controller.Interface --load "dataset.filename=/data/datasets/$DATASET,saturation.enable=$SATURATE" | tee -a /data/datasets/$DATASET.log
+java -Xmx90g -jar target/RDFQuotient-1.8-SNAPSHOT-with-dependencies.jar --load "dataset.filename=/data/datasets/$DATASET,saturation.enable=$SATURATE" | tee -a /data/datasets/$DATASET.log
