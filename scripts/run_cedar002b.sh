394c05e49c3e6c0ec1884cc9a2cd10006c1921ce
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2019-09-12 18:36:33 +0200

    Changing scripts and cluster dataset allocations; fixing a tiny bug in drawing: a field initialized statically instead of initialization in the constructor
    Former-commit-id: ee5ff462f628bd18246b93e103093e3155ffc31b

--- scripts/run_cedar002b.sh
+++ scripts/run_cedar002b.sh
@@ -2,7 +2,7 @@
 
 for DATASET in bsbm/bsbm100m.nt bsbm/bsbm138m.nt; do
 	for SATURATED in "true" "false"; do
-		for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb onefw; do
+		for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb onefw typed inputoutput bisim; do
 			./scripts/summarize.sh $DATASET $SUMMARY_TYPE $SATURATED false split_and_fold_leaves
 		done
 	done
--- scripts/run_cedar003.sh
+++ scripts/run_cedar003.sh
@@ -2,7 +2,7 @@
 
 for DATASET in bsbm/bsbm1m.nt bsbm/bsbm10m.nt dbpedia/dbpedia_persondata_en_uniq.nt lubm/lubm1m.nt lubm/lubm10m.nt watdiv/watdiv10m_onto.nt; do
 	for SATURATED in "true" "false"; do
-		for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb onefw; do
+		for SUMMARY_TYPE in weak strong typedweak typedstrong 2pweak 2pstrong 2ptypedweak 2ptypedstrong 2pweakunionfind onefb onefw typed inputoutput bisim; do
 			./scripts/summarize.sh $DATASET $SUMMARY_TYPE $SATURATED false split_and_fold_leaves
 		done
 	done
