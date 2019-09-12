805c55790cc8eee14b3d97c44159454c17aa31af
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2019-09-12 10:12:39 +0200

    Updating scripts
    Former-commit-id: 5318ae8d020af762db9a072e3f310de035430443

--- scripts/summarize.sh
+++ scripts/summarize.sh
@@ -7,5 +7,5 @@ TYPE_GENERALIZATION=$4
 DRAW=$5
 
 # timeout set to 1 hour and memory allocation in JVM set to 90 GiB
-#timeout 1h java -Xmx90g -cp target/RDFQuotient-1.8-SNAPSHOT-with-dependencies.jar fr.inria.cedar.RDFQuotient.controller.Interface --summarize "dataset.filename=/data/datasets/$DATASET,summary.type=$SUMMARY_TYPE,summary.summarize_saturated_graph=$SATURATED,summary.replace_type_with_most_general_type=$TYPE_GENERALIZATION,drawing.style=$DRAW" | tee -a /data/datasets/$DATASET.log
-timeout 1h java -Xmx90g -jar target/RDFQuotient-1.8-SNAPSHOT-with-dependencies.jar --summarize "dataset.filename=/data/datasets/$DATASET,summary.type=$SUMMARY_TYPE,summary.summarize_saturated_graph=$SATURATED,summary.replace_type_with_most_general_type=$TYPE_GENERALIZATION,drawing.style=$DRAW" | tee -a /data/datasets/$DATASET.log
+#timeout 1h java -Xmx90g -cp target/RDFQuotient-1.8-with-dependencies.jar fr.inria.cedar.RDFQuotient.controller.Interface --summarize "dataset.filename=/data/datasets/$DATASET,summary.type=$SUMMARY_TYPE,summary.summarize_saturated_graph=$SATURATED,summary.replace_type_with_most_general_type=$TYPE_GENERALIZATION,drawing.style=$DRAW" | tee -a /data/datasets/$DATASET.log
+timeout 1h java -Xmx90g -jar target/RDFQuotient-1.8-with-dependencies.jar --summarize "dataset.filename=/data/datasets/$DATASET,summary.type=$SUMMARY_TYPE,summary.summarize_saturated_graph=$SATURATED,summary.replace_type_with_most_general_type=$TYPE_GENERALIZATION,drawing.style=$DRAW" | tee -a /data/datasets/$DATASET.log
