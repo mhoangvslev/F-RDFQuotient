103bf5027fae11f4504b5a3f303ac41d479384bc
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2019-10-01 10:31:17 +0200

    Update to a script
    Former-commit-id: c70bdf8ac3048b9ab40bd7fdef3c0a4f4153c499

--- scripts/run_cedar001_shortcut.sh
+++ scripts/run_cedar001_shortcut.sh
@@ -17,7 +17,7 @@ translate_summary_name() {
 }
 
 for SUMMARY_TYPE in weak strong 2pweak 2pstrong 2pweakunionfind 2ponefb; do
-	for DATASET in springer/conference.nt nobel/nobel.nt insee/insee_geo.nt; do
+	for DATASET in springer/conference.nt nobel/nobel.nt nobelprizes/nobelprizes_uniq.nt insee/insee_geo.nt; do
 		# SATURATE + SUMMARIZE
 		# summarize saturated
 		./scripts/summarize.sh $DATASET $SUMMARY_TYPE true false false
