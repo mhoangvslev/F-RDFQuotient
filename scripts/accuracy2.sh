67bc4eec3bd54d2ff360dde573e2d39a11f6fab2
Paweł Guzewicz <pawel.guzewicz@inria.fr>
2019-10-07 09:12:40 +0200

    Minor fix to the accuracy script
    Former-commit-id: 5070f9499b2fb94b06f680f4ac56bd3ce9e03d97

--- scripts/accuracy2.sh
+++ scripts/accuracy2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-logfile=$1"_Accuracy_2".log
+logfile=$1"_accuracy2".log
 
 mvn exec:java -Dexec.mainClass=fr.inria.cedar.RDFQuotient.analysis.Accuracy -Dexec.args="$*" | tee -a $logfile
