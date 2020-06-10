11ab49c5356d25f85143baea4f9b819bbe4f8a31
GUZEWICZ Pawel <pawel.guzewicz@inria.fr>
2020-06-10 12:09:45 +0200

    Updating scripts for loading with saturation
    Former-commit-id: 25c848b1b1ce6cc1d38c08ad6c574208b96f2d21

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DATASET=$1
-SATURATE=$2
+SATURATION_TYPE=$2
 
 # memory allocation in JVM set to 90 GiB
 #java -Xmx90g -cp target/RDFQuotient-1.9-SNAPSHOT-with-dependencies.jar fr.inria.cedar.RDFQuotient.controller.Interface --load "dataset.filename=/data/datasets/$DATASET,saturation.enable=$SATURATE" | tee -a /data/datasets/$DATASET.log
-java -Xmx90g -jar target/RDFQuotient-1.9-SNAPSHOT-with-dependencies.jar --load "dataset.filename=/data/datasets/$DATASET,saturation.enable=$SATURATE" | tee -a /data/datasets/$DATASET.log
+java -Xmx90g -jar target/RDFQuotient-1.9-SNAPSHOT-with-dependencies.jar --load "dataset.filename=/data/datasets/$DATASET,saturation.type=$SATURATION_TYPE" | tee -a /data/datasets/$DATASET.log
--- scripts/run_cedar007_shortcut.sh
+++ scripts/run_cedar007_shortcut.sh
@@ -28,7 +28,7 @@ for SUMMARY_TYPE in weak strong 2pweak 2pstrong 2pweakunionfind 2ponefb; do
 		DATASET=${DATASET%/*}\/summariesNT\/${DATASET##*/}
 		DATASET=${DATASET%.*}\_$(translate_summary_name $SUMMARY_TYPE).nt
 		# load the summary with saturation
-		./scripts/load.sh $DATASET true
+		./scripts/load.sh $DATASET RDFS_SAT
 		# summarize saturated
 		./scripts/summarize.sh $DATASET $SUMMARY_TYPE true false split_and_fold_leaves
 	done
